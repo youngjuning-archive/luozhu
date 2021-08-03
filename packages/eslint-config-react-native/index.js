80dfbfe1f85158266c4792a006b16cd2287608a4
杨俊宁 <youngjuning@aliyun.com>
2021-08-04 02:51:23 +0800

    feat: add @luozhu/eslint-config-react-native

---
+++ packages/eslint-config-react-native/index.js
@@ -0,0 +1,14 @@
+require('./patch');
+
+module.exports = {
+  extends: ['plugin:react-native/all', '@luozhu/eslint-config-react-typescript'],
+  env: {
+    'react-native/react-native': true,
+  },
+  rules: {
+    'react-native/no-color-literals': 0,
+    'react-native/split-platform-components': 0,
+    'react-native/no-raw-text': 0,
+    'react-native/no-inline-styles': 1,
+  },
+};

7f4f1ae3e206312b6179a83bec18267dca983961
杨俊宁 <youngjuning@aliyun.com>
2021-08-04 00:23:48 +0800

    feat: add eslint-import-resolver-typescript

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -5,11 +5,27 @@ module.exports = {
   overrides: [
     {
       files: ['*.ts'],
-      extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+      extends: [
+        'airbnb-base',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+      ],
       parser: '@typescript-eslint/parser',
       rules: {
         ...require('@luozhu/eslint-config-base/rule'),
         ...require('./rule'),
+      },
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+          // use <root>/tsconfig.json
+          typescript: {
+            alwaysTryTypes: true, // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
+          },
+        },
       },
     },
   ],

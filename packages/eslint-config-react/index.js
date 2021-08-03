4fe47ebd44f4501eed7204b673252a87f72d86fc
杨俊宁 <youngjuning@aliyun.com>
2021-08-04 01:13:37 +0800

    feat: add @luozhu/eslint-config-react

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,28 @@
+require('./patch');
+
+module.exports = {
+  extends: ['@luozhu/eslint-config-typescript'],
+  overrides: [
+    {
+      files: ['*.jsx'],
+      extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended', 'prettier'],
+      parser: '@babel/eslint-parser',
+      parserOptions: {
+        ecmaFeatures: {
+          impliedStrict: true,
+          jsx: true,
+        },
+        ecmaVersion: 2020,
+      },
+      rules: {
+        ...require('@luozhu/eslint-config-base/rule'),
+        ...require('./rule'),
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    },
+  ],
+};

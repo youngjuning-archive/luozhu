40922be991c8a68609ac701602a8ed00578f7dea
洛竹 <youngjuning@aliyun.com>
2021-08-04 15:35:09 +0800

    feat: remove @babel/eslint-parser

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -5,15 +5,10 @@ module.exports = {
     {
       files: ['*.jsx', '*.js'],
       extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended', 'prettier'],
-      parser: '@babel/eslint-parser',
       parserOptions: {
         ecmaFeatures: {
           impliedStrict: true,
           jsx: true,
-        },
-        requireConfigFile: false,
-        babelOptions: {
-          presets: ['@babel/preset-react'],
         },
         ecmaVersion: 2020,
       },

91f01cf6339562770b8d6cca8f3701323c10e27f
杨俊宁 <youngjuning@aliyun.com>
2021-08-05 00:35:49 +0800

    chore: add create-luozhu-package

---
+++ packages/create-luozhu-package/src/index.ts
@@ -0,0 +1,16 @@
+import { program } from 'commander';
+import fs from 'fs-extra';
+import path from 'path';
+
+const init = (): void => {
+  const packageJson = require('../package.json');
+  program.version(packageJson.version).description(packageJson.description);
+  fs.readdirSync(path.join(__dirname, 'commands')).forEach((file: string) => {
+    if (!file.endsWith('.js')) return;
+    // eslint-disable-next-line import/no-dynamic-require
+    require(path.join(__dirname, 'commands', file));
+  });
+  program.parse(process.argv);
+};
+
+export = { init };

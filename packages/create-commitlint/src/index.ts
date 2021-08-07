5ee09a88284275575314bf906963a7501fc6c9f5
杨俊宁 <youngjuning@aliyun.com>
2021-08-07 12:39:40 +0800

    feat: add @luozhu/create-commitlint

---
+++ packages/create-commitlint/src/index.ts
@@ -0,0 +1,44 @@
+import fs from 'fs-extra';
+import path from 'path';
+import shell from 'shelljs';
+
+module.exports = async () => {
+  const templateDir = path.resolve(__dirname, '../templates/conventional');
+  const projectDir = `${process.cwd()}`;
+  fs.copySync(templateDir, projectDir);
+  console.log();
+  const origin = JSON.parse(fs.readFileSync(`${projectDir}/package.json`, 'utf8'));
+
+  const config = {
+    ...origin,
+    scripts: {
+      ...origin.scripts,
+      commit: 'git cz',
+    },
+    config: origin.config
+      ? {
+          ...origin.config,
+          commitizen: {
+            path: 'cz-customizable',
+          },
+        }
+      : {
+          commitizen: {
+            path: 'cz-customizable',
+          },
+        },
+    gitHooks: origin.gitHooks
+      ? {
+          ...origin.gitHooks,
+          'commit-msg': 'commitlint -e -V',
+        }
+      : {
+          'commit-msg': 'commitlint -e -V',
+        },
+  };
+
+  fs.writeFileSync(`${projectDir}/package.json`, JSON.stringify(config));
+  shell.exec(
+    'yarn add commitizen cz-customizable @commitlint/cli commitlint-config-cz yorkie -D -W'
+  );
+};

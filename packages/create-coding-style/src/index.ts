db6e7815837aa4bc832f74b389b1bc2869d641eb
洛竹 <youngjuning@aliyun.com>
2021-09-13 00:45:26 +0800

    feat(create-coding-style): add lint-stage and vscode settings

--- packages/create-coding-style/src/index.ts
+++ packages/create-coding-style/src/index.ts
@@ -9,6 +9,43 @@ const init = async () => {
   const projectDir = `${process.cwd()}`;
   fs.copySync(templateDir, projectDir);
   console.log();
+  // 拼接 package.json 文件
+  const origin = JSON.parse(fs.readFileSync(`${projectDir}/package.json`, 'utf8'));
+  const packageJson = {
+    ...origin,
+    gitHooks: origin.gitHooks
+      ? {
+          ...origin.gitHooks,
+          'pre-commit': 'lint-staged',
+        }
+      : {
+          'pre-commit': 'lint-staged',
+        },
+    'lint-staged': origin['lint-staged']
+      ? {
+          ...origin.gitHooks,
+          '**/*.{js,jsx,ts,tsx}': ['eslint --fix'],
+          '**/*.{md,json}': ['prettier --write'],
+        }
+      : {
+          '**/*.{js,jsx,ts,tsx}': ['eslint --fix'],
+          '**/*.{md,json}': ['prettier --write'],
+        },
+  };
+  fs.writeFileSync(`${projectDir}/package.json`, JSON.stringify(packageJson));
+  // 拼接 .vscode/settings.json 文件
+  const originVscodeSettings = JSON.parse(
+    fs.readFileSync(`${projectDir}/.vscode/settings.json`, 'utf8')
+  );
+  const vscodeSettings = {
+    ...originVscodeSettings,
+    'editor.formatOnSave': false,
+    'editor.codeActionsOnSave': {
+      'source.fixAll.eslint': true,
+    },
+  };
+  fs.writeFileSync(`${projectDir}/.vscode/settings.json`, JSON.stringify(vscodeSettings));
+  // 拼接 .eslintrc.js 文件
   const prompt = new Select({
     name: 'eslintType',
     message: 'Pick a eslint type',
@@ -28,6 +65,7 @@ const init = async () => {
 };`;
 
   fs.writeFileSync(`${projectDir}/.eslintrc.js`, eslintConfig);
+  // 安装依赖
   execa.commandSync(
     `yarn add commitizen -D -W eslint prettier @luozhu/eslint-config-${eslintType} @luozhu/prettier-config lint-staged yorkie`,
     {

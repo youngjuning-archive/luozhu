3fa41caa66b27521ba7a710639e103cdcff3c093
洛竹 <youngjuning@aliyun.com>
2021-09-14 21:00:43 +0800

    chore: add some eslint rule

--- packages/create-vscode-extension/template/.eslintrc.js
+++ packages/create-vscode-extension/template/.eslintrc.js
@@ -2,4 +2,9 @@ module.exports = {
   root: true,
   extends: ['@luozhu/eslint-config-typescript'],
   ignorePatterns: ['out'],
+  rules: {
+    'no-console': 0,
+    'no-useless-constructor': 0,
+    'no-param-reassign': 0,
+  },
 };

220b3c979fd1a4f9f1ef6658be43ff6a7d4a3bea
洛竹 <youngjuning@aliyun.com>
2021-09-16 10:23:40 +0800

    Revert "fix: remove window.vscodeEnv for using-proposed-api"
    This reverts commit ca410a9624dc62c40c4b7e66eff3e116d34336a1.

--- packages/create-vscode-webview/template/web/src/layouts/index.tsx
+++ packages/create-vscode-webview/template/web/src/layouts/index.tsx
@@ -2,7 +2,6 @@ import { setLocale } from 'umi';
 
 require('./index.less');
 
-// TODO: 重构 vscode env 的获取方式
 setLocale(window.vscodeEnv.language === 'zh-cn' ? 'zh-CN' : window.vscodeEnv.language, false);
 
 export default props => {

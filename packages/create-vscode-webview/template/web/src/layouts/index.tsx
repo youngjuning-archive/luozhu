ca410a9624dc62c40c4b7e66eff3e116d34336a1
洛竹 <youngjuning@aliyun.com>
2021-09-16 09:57:42 +0800

    fix: remove window.vscodeEnv for using-proposed-api

--- packages/create-vscode-webview/template/web/src/layouts/index.tsx
+++ packages/create-vscode-webview/template/web/src/layouts/index.tsx
@@ -2,6 +2,7 @@ import { setLocale } from 'umi';
 
 require('./index.less');
 
+// TODO: 重构 vscode env 的获取方式
 setLocale(window.vscodeEnv.language === 'zh-cn' ? 'zh-CN' : window.vscodeEnv.language, false);
 
 export default props => {

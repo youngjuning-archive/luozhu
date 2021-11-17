7388902aa44d852c6fc3907074224150a0ae8496
洛竹 <youngjuning@163.com>
2021-11-17 19:20:59 +0800

    feat: 新增命令

--- packages/geita/README.md
+++ packages/geita/README.md
@@ -8,7 +8,7 @@ $ npm install @luozhu/geita -g
 
 ## 命令
 
-### geita prune <file>
+### geita prune-file <file>
 
 单独清理某个文件的所有历史记录。
 
@@ -19,13 +19,10 @@ git push origin --force --all
 git push origin --force --tags
 ```
 
-5. 确保没有什么问题之后,强制解除对本地存储库中的所有对象的引用和垃圾收集
+确保没有什么问题之后,强制解除对本地存储库中的所有对象的引用和垃圾收集：
 
-git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
-git reflog expire --expire=now --all
+```sh
+git gc --prune=now
+```
 
-————————————————
-版权声明：本文为 CSDN 博主「JAVA|Mr.Java」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/q258523454/article/details/83899911
-
-- git gc --prune=now
+### geita prune-msg <msg>
--- packages/geita/src/index.ts
+++ packages/geita/src/index.ts
@@ -7,8 +7,8 @@ export const init = () => {
   program.version(pkgJson.version).description(pkgJson.description);
 
   program
-    .command('prune <file>')
-    .description('start named service')
+    .command('prune-file <file>')
+    .description('单独清理某个文件的所有历史记录。')
     .action(file => {
       try {
         execa.command(
@@ -23,6 +23,22 @@ export const init = () => {
         console.log(error.message);
       }
     });
-
+  program
+    .command('prune-msg <msg>')
+    .description('清理 commit message 中的敏感信息。')
+    .action(msg => {
+      try {
+        execa.command(
+          `FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --msg-filter "perl -CIOED -p -e 's/${msg}/ /g'" -- --all`,
+          {
+            shell: true,
+            stdout: 'inherit',
+            stderr: 'inherit',
+          }
+        );
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    });
   program.parse(process.argv);
 };
